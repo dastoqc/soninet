63d28cb0cd5c600e712826a6c79fa097c81703d5
dave <david.st-onge@polymtl.ca>
2019-07-24 00:56:37 -0400

    integrated file pitch variations

--- csvtosound.py
+++ csvtosound.py
@@ -5,10 +5,65 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pygame, pygame.sndarray
 
-sampleRate = 44100
-# 44.1kHz, 16-bit signed, mono
-pygame.mixer.pre_init(sampleRate, -16, 1)
-pygame.init()
+class CreateSound(object):
+    def __init__(self):
+        self.sampleRate = 44100
+        # 44.1kHz, 16-bit signed, mono
+        pygame.mixer.pre_init(self.sampleRate, -16, 1)
+        pygame.init()
+        self.channels = [[pygame.mixer.Channel(1),None,-1],[pygame.mixer.Channel(2),None,-1]]
+        self.snds = [pygame.mixer.Sound('audio_files/bell001.flac'),pygame.mixer.Sound('audio_files/bell002.flac')]
+
+    def speedx(self, snd_array, factor):
+        """ Speeds up / slows down a sound, by some factor. """
+        indices = np.round(np.arange(0, len(snd_array), factor))
+        indices = indices[indices < len(snd_array)].astype(int)
+        return snd_array[indices]
+
+    def stretch(self, snd_array, factor, window_size, h):
+        """ Stretches/shortens a sound, by some factor. """
+        phase = np.zeros(window_size)
+        hanning_window = np.hanning(window_size)
+        result = np.zeros(int(len(snd_array) / factor + window_size))
+
+        for i in np.arange(0, len(snd_array) - (window_size + h), h*factor):
+            i = int(i)
+            # Two potentially overlapping subarrays
+            a1 = snd_array[i: i + window_size]
+            a2 = snd_array[i + h: i + window_size + h]
+
+            # The spectra of these arrays
+            s1 = np.fft.fft(hanning_window * a1)
+            s2 = np.fft.fft(hanning_window * a2)
+
+            # Rephase all frequencies
+            phase = (phase + np.angle(s2/s1)) % 2*np.pi
+
+            a2_rephased = np.fft.ifft(np.abs(s2)*np.exp(1j*phase))
+            i2 = int(i/factor)
+            result[i2: i2 + window_size] += hanning_window*a2_rephased.real
+
+        # normalize (16bit)
+        result = ((2**(16-4)) * result/result.max())
+
+        return result.astype('int16')
+
+    def pitchshift(self, snd_array, n, window_size=2**13, h=2**11):
+        """ Changes the pitch of a sound by ``n`` semitones. """
+        factor = 2**(1.0 * n / 12.0)
+        stretched = self.stretch(snd_array, 1.0/factor, window_size, h)
+        return self.speedx(stretched[window_size:], factor)
+
+    def play(self, vol, semitone, chan):
+        channel_tuple = self.channels[chan]
+        if channel_tuple[0].get_busy() != 1:
+            channel_tuple[1] = pygame.mixer.Sound(self.pitchshift(pygame.sndarray.array(self.snds[chan]), semitone))
+            channel_tuple[1].set_volume(vol / 100.0)
+            try:
+                channel_tuple[0].play(channel_tuple[1])
+            except:
+                pass
+
 
 class AnimatedScatter(object):
     """An animated scatter plot using matplotlib.animations.FuncAnimation."""
@@ -18,8 +73,7 @@ class AnimatedScatter(object):
         self.hop1all = hop1all
         self.hop2all = hop2all
         self.selectID = 0
-        self.channels = [[pygame.mixer.Channel(1),None,-1],[pygame.mixer.Channel(2),None,-1]]
-
+        self.CS = CreateSound()
         # Setup the figure and axes...
         self.fig, self.ax = plt.subplots()
         # Then setup FuncAnimation.
@@ -36,25 +90,13 @@ class AnimatedScatter(object):
 
     def updatesound(self, cnt):
         # Scale the values to fit sine wave generation
-        freq1 = self.hop1all[self.selectID][cnt][0]*35+120
+        semitone1 = self.hop1all[self.selectID][cnt][0]
         vol1 = 8000 - self.hop1all[self.selectID][cnt][1]*50
-        freq2 = self.hop2all[self.selectID][cnt][0]*35+120
+        semitone2 = self.hop2all[self.selectID][cnt][0]
         vol2 = 8000 - self.hop2all[self.selectID][cnt][1]*50
-        print(vol1, freq1, vol2, freq2)
-        # generation the sine wave data
-        arr1 = np.array([vol1 * np.sin(2.0 * np.pi * freq1 * x / sampleRate) for x in range(0, int(1.4*sampleRate))]).astype(np.int16)
-        arr2 = np.array([vol2 * np.sin(2.0 * np.pi * freq2 * x / sampleRate) for x in range(0, int(1.4*sampleRate))]).astype(np.int16)
-        # play (2 channels avalaible)
-        for index, channel_tuple in enumerate(self.channels):
-            if channel_tuple[0].get_busy() != 1:
-                if(index==0):
-                    channel_tuple[1] = pygame.sndarray.make_sound(arr1)
-                else:
-                    channel_tuple[1] = pygame.sndarray.make_sound(arr2)
-                try:
-                    channel_tuple[0].play(channel_tuple[1])
-                except:
-                    pass
+        print(vol1, semitone1, vol2, semitone2)
+        self.CS.play(vol1, semitone1, 0)
+        self.CS.play(vol2, semitone2, 1)
 
     def update(self, cnt):
         """Update the scatter plot."""
@@ -76,52 +118,57 @@ class AnimatedScatter(object):
 def Average(lst):
     return sum(lst) / len(lst)
 
-src_csv = 'log/Robot_'
-posall = {}
-hop1all = {}
-hop2all = {}
 
-for rid in range(0,10):
-    csvfile = open(src_csv+str(rid), 'r')
-    reader = csv.reader(csvfile, delimiter=',', lineterminator='\n')
-    row_count = sum(1 for row in reader)
-    print(str(rid), " has ", row_count, " lines")
-    csvfile.seek(0)
-    posall[rid]=[]
-    hop1all[rid]=[]
-    hop2all[rid]=[]
-    i = 0
+if __name__ == "__main__":
 
-    for row in reader:
-        posall[rid].append([float(row[0]),float(row[1])])
-        x = []
-        y = []
-        if int(row[3])>0:
-            for nei in range(0,int(row[3])):
-                x.append(float(row[3+nei*4+2]))
-                y.append(float(row[3+nei*4+3]))
-                #print(x,y)
-            ave = math.sqrt(Average(x)*Average(x)+Average(y)*Average(y))
-        else:
-            ave = 0
-        hop1all[rid].append([int(row[3]),ave])
-        #print(int(row[3]))
-        if(len(row)>12):
-            #print(i,len(row),int(row[3]),int(row[4+int(row[3])*4]))
-            if int(row[4+int(row[3])*4])>0:
-                x = []
-                y = []
-                for nei in range(0,int(row[4+int(row[3])*4])):
+    src_csv = 'log/Robot_'
+    posall = {}
+    hop1all = {}
+    hop2all = {}
+
+    # Parse the CSVs
+    for rid in range(0,10):
+        csvfile = open(src_csv+str(rid), 'r')
+        reader = csv.reader(csvfile, delimiter=',', lineterminator='\n')
+        row_count = sum(1 for row in reader)
+        print(str(rid), " has ", row_count, " lines")
+        csvfile.seek(0)
+        posall[rid]=[]
+        hop1all[rid]=[]
+        hop2all[rid]=[]
+        i = 0
+
+        for row in reader:
+            posall[rid].append([float(row[0]),float(row[1])])
+            x = []
+            y = []
+            if int(row[3])>0:
+                for nei in range(0,int(row[3])):
                     x.append(float(row[3+nei*4+2]))
                     y.append(float(row[3+nei*4+3]))
                     #print(x,y)
                 ave = math.sqrt(Average(x)*Average(x)+Average(y)*Average(y))
             else:
                 ave = 0
-            hop2all[rid].append([int(row[4+int(row[3])*4]),ave])
-        else:
-            hop2all[rid].append([0,0])
-        i = i + 1
-#print(hop2all)
-a = AnimatedScatter(posall,hop1all,hop2all)
-plt.show()
+            hop1all[rid].append([int(row[3]),ave])
+            #print(int(row[3]))
+            if(len(row)>12):
+                #print(i,len(row),int(row[3]),int(row[4+int(row[3])*4]))
+                if int(row[4+int(row[3])*4])>0:
+                    x = []
+                    y = []
+                    for nei in range(0,int(row[4+int(row[3])*4])):
+                        x.append(float(row[3+nei*4+2]))
+                        y.append(float(row[3+nei*4+3]))
+                        #print(x,y)
+                    ave = math.sqrt(Average(x)*Average(x)+Average(y)*Average(y))
+                else:
+                    ave = 0
+                hop2all[rid].append([int(row[4+int(row[3])*4]),ave])
+            else:
+                hop2all[rid].append([0,0])
+            i = i + 1
+
+    # Start the scatter plot animation with sound
+    a = AnimatedScatter(posall,hop1all,hop2all)
+    plt.show()
